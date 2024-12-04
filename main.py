e28ad6ac0be0372b1064fe62e37549bee034c5cf
yeongseo <115892001+zeroseoS2@users.noreply.github.com>
2024-12-04 14:15:21 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True) 
+
+
+from langchain_openai import ChatOpenAI
+chat_model = ChatOpenAI()
+
+# subject="AI"
+# result = chat_model.invoke(subject + "에 대한 시를 써줘.")
+# print(result.content)
+
+import streamlit as st
+st.title("인공지능 시인")
+subject=st.text_input("시의 주제를 입력해주세요.")
+st.write("시의 주제 : " + subject)
+
+if st.button("시 작성"):
+    with st.spinner("시 작성중 ..."):
+        result = chat_model.invoke( subject + "에 대한 시를 써줘")
+        st.write(result.content)
